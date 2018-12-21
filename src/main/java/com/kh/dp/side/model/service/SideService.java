1de33f1a44165d4292cb918df4e44ffd0b7b813c
Ssongs2 <sejeq1995@gmail.com>
2018-12-21 15:15:26 +0900

    프로젝트 매칭 요청
    프로젝트 요청 멤버
    불러오기/
    스케줄 매칭 요청 정보
    DB저장/
    매칭날짜 최소 1일 ~ 7일 제약걸기/
    현재 날짜 이후로만 선택 가능하도록/

--- src/main/java/com/kh/dp/side/model/service/SideService.java
+++ src/main/java/com/kh/dp/side/model/service/SideService.java
@@ -3,9 +3,12 @@ package com.kh.dp.side.model.service;
 import java.util.List;
 
 import com.kh.dp.member.model.vo.Member;
+import com.kh.dp.side.model.vo.Matching;
 
 public interface SideService {
 
 	public List<Member> browseMember();
+
+	public int insertMatching(Matching matcing);
 
 }
