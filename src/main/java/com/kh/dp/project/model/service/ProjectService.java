9f9ab8240a1a9fb81e6291dee7b3ecfab53e288b
jds0401 <yusim22@naver.com>
2018-12-26 14:13:34 +0900

    project memo

--- src/main/java/com/kh/dp/project/model/service/ProjectService.java
+++ src/main/java/com/kh/dp/project/model/service/ProjectService.java
@@ -1,5 +1,13 @@
 package com.kh.dp.project.model.service;
 
-public interface ProjectService {
+import java.util.List;
+import java.util.Map;
 
+import com.kh.dp.project.model.vo.Memo;
+
+public interface ProjectService {
+	
+	List<Map<String, String>> selectMemoList();
+
+	int updateMemo(String saveMemo);
 }
