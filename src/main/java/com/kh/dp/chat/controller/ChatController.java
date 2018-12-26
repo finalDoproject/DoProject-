0b26c3f41137cbe5e3a99bda3e043b496e8c5574
sjk1992 <sjk1992@nate.com>
2018-12-26 11:08:22 +0900

    2018/12/26 commit 2

--- src/main/java/com/kh/dp/chat/controller/ChatController.java
+++ src/main/java/com/kh/dp/chat/controller/ChatController.java
@@ -3,7 +3,6 @@ package com.kh.dp.chat.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ChatController {
