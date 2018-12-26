10706196b2f98c40919f1954937c3e8c0dfc0574
Ssongs2 <sejeq1995@gmail.com>
2018-12-26 15:17:17 +0900

    요청에 대한 답 DB (Ajax 사용)
    ㅇㅇ

--- src/main/java/com/kh/dp/side/model/service/SideService.java
+++ src/main/java/com/kh/dp/side/model/service/SideService.java
@@ -3,6 +3,7 @@ package com.kh.dp.side.model.service;
 import java.util.List;
 
 import com.kh.dp.member.model.vo.Member;
+import com.kh.dp.side.model.vo.Join;
 import com.kh.dp.side.model.vo.Matching;
 
 public interface SideService {
@@ -10,5 +11,7 @@ public interface SideService {
 	public List<Member> browseMember();
 
 	public int insertMatching(Matching matcing);
+	
+	public int insertMatchingDT(Join join);
 
 }
