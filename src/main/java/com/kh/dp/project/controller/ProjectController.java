5612e5fdb997932eb7dab07c0e337a9b35a7474c
helloforsemi <>
2018-12-26 14:57:23 +0900

    Revert "conflicts add"
    This reverts commit 7816a396932d7257f84818fb81e9c162817f62be, reversing
    changes made to 2798c71ef6aa5222eae8f8c547b10e17191f3a39.

--- src/main/java/com/kh/dp/project/controller/ProjectController.java
+++ src/main/java/com/kh/dp/project/controller/ProjectController.java
@@ -1,28 +1,11 @@
 package com.kh.dp.project.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.kh.dp.project.model.service.ProjectService;
-import com.kh.dp.project.model.vo.Memo;
 
 @Controller
 public class ProjectController {
-	
-	@Autowired
-	ProjectService projectService;
-	
+
 	@RequestMapping("/project/projectMain.do")
 	public String ProjectView() {
 		
@@ -30,28 +13,8 @@ public class ProjectController {
 	}
 	
 	@RequestMapping("/project/projectPage.do")
-	public String ProjectPageView(Model model) {
+	public String ProjectPageView() {
 		
-		List<Map<String,String>> memoList = projectService.selectMemoList();
-		model.addAttribute("memoList",memoList);
-		
-		return "project/projectPage";
+		return null;
 	}
-	
-	@RequestMapping(value="/project/projectPage.do", method=RequestMethod.POST)
-	@ResponseBody
-	public Map<String,Object> updateMemo(@RequestParam(value="saveMemo", required=false) String saveMemo){
-		System.out.println("메모:" + saveMemo);
-		String msg  = projectService.updateMemo(saveMemo)>0?"수정 성공":"수정 실패";		
-		//model.addAttribute("msg",msg);
-		Map<String, Object> hmap = new HashMap<>();
-		
-		hmap.put("msg", msg);
-		
-		
-		return hmap;
-		
-	}
-	
-
 }
--- src/main/java/com/kh/dp/project/model/service/ProjectService.java
+++ src/main/java/com/kh/dp/project/model/service/ProjectService.java
@@ -1,13 +1,5 @@
 package com.kh.dp.project.model.service;
 
-import java.util.List;
-import java.util.Map;
-
-import com.kh.dp.project.model.vo.Memo;
-
 public interface ProjectService {
-	
-	List<Map<String, String>> selectMemoList();
 
-	int updateMemo(String saveMemo);
 }
