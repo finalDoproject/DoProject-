04d6cb27ec2cc7d065e90ce74d7a54d136258a3b
sjk1992 <sjk1992@nate.com>
2019-01-16 14:55:09 +0900

    commit

---
+++ src/main/java/com/kh/dp/chat/controller/ChatCountController.java
@@ -0,0 +1,76 @@
+package com.kh.dp.chat.controller;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.kh.dp.chat.model.service.ChatService;
+import com.kh.dp.member.model.vo.Member;
+import com.kh.dp.project.model.vo.Project;
+
+@Controller
+public class ChatCountController {
+	
+	@Autowired
+	private ChatService chatService;
+	
+	@RequestMapping("/countSSE")
+	public ResponseBodyEmitter handleRequest(HttpSession session) {
+		
+		Member m = (Member)session.getAttribute("member");
+		Project p = (Project)session.getAttribute("project");
+		@SuppressWarnings("unchecked")
+		ArrayList<Member> list = (ArrayList<Member>)session.getAttribute("secondList");
+		
+		System.out.println("m : " + m);
+		System.out.println("p : " + p);
+		System.out.println("list : " + list);
+		for(Member me : list) {
+			for(Member you : list) {
+				if(me != you) {
+					System.out.println(me.toString() + " : " + you.toString());
+				}
+			}
+		}
+
+		final SseEmitter emitter = new SseEmitter();
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		service.execute(() -> {
+			for(int i = 0; i < 500; i++) {
+				try {
+					String ptm = chatService.selectOneChatPtm(m.getNickName(), p.getPno());
+					emitter.send("p"+p.getPno()+":"+ptm, MediaType.TEXT_PLAIN);
+					String mtm = null;
+					for(Member me : list) {
+						for(Member you : list) {
+							if(me != you) {
+								mtm = chatService.selectOneChatMtm(me.getNickName(), p.getPno(), me.getMno(), you.getMno());
+								emitter.send("m"+me.getMno()+"TOm"+you.getMno()+":"+mtm, MediaType.TEXT_PLAIN);
+							}
+						}
+					}
+					/*emitter.send(String.valueOf(i), MediaType.TEXT_PLAIN);*/
+					Thread.sleep(200);
+				} catch(Exception e) {
+					e.printStackTrace();
+					emitter.completeWithError(e);
+					return;
+				}
+			}
+			emitter.complete();
+		});
+		return emitter;
+	}
+	
+}
